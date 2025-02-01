4d80110e186f394d075d736574c18badf0cf03e6
DAGBOYI <128738072+dagboyidavid@users.noreply.github.com>
2025-02-01 05:36:26 +0100

    Create DAGBOYI.java
    Good day sir, not clear on the instructions to the project whether adding my name to the contributors folder was just fin, so I thought to upload d code here too.

---
+++ DAGBOYI.java
@@ -0,0 +1,29 @@
+public class DAGBOYI{
+//Dagboyi David Onuche-Ojo U23ME1028 Mechanicak Engineering 
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        DAGBOYI calculator = new DAGBOYI();
+        System.out.println("Addition: " + calculator.add(2, 3));
+        System.out.println("Subtraction: " + calculator.subtract(5, 2));
+        System.out.println("Multiplication: " + calculator.multiply(3, 4));
+        System.out.println("Division: " + calculator.divide(10, 2));
+    }
+}
